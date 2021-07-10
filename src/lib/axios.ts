7c084280a52f82ac335580bf1c3fa04d38320991
矢野大樹 <daikitech0123@gmail.com>
2021-07-10 20:35:43 +0900

    本番用のbaseURL設定

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,5 @@
 import axios from 'axios'
 const development = process.env.NODE_ENV !== 'production'
 export default axios.create({
-  baseURL: development ? 'http://localhost:3000/' : 'https://domain/api'
+  baseURL: development ? 'http://localhost:3000/' : 'https://prohub-api.herokuapp.com/'
 })
