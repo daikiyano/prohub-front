bd3301a94056bceb2f878dafe334e8b494ba5e80
矢野大樹 <daikitech0123@gmail.com>
2021-07-20 23:14:47 +0900

    vue-rating追加、設定

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="nav">
-    <Header />
+    <AdminHeader v-if="isAdmin" />
+    <Header v-if="!isAdmin" />
     <!-- <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> -->
   </div>
@@ -9,19 +10,30 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import AdminHeader from '@/components/AdminHeader.vue'; // @ is an alias to /src
 import Header from '@/components/Header.vue'; // @ is an alias to /src
+
 
 export default defineComponent({
   name: 'App',
   components: {
-    Header,
+    AdminHeader,
+    Header
   },
+  setup() {
+    const isAdmin = localStorage.getItem('admin')
+    return {isAdmin}
+  }
  
 });
 </script>
 
 
 <style>
+
+body {
+  background-color: #FAFAFA;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
